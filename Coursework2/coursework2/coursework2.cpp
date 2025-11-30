c9384f98ab217952a887ab9dd4948f86289d450e
JamHam04 <james.hampson2004@gmail.com>
2025-11-30 14:10:58 +0000

    CreateNewObject Function
    Function to pass vertices and indices through a function

--- Coursework2/coursework2/coursework2.cpp
+++ Coursework2/coursework2/coursework2.cpp
@@ -12,15 +12,55 @@ using namespace glm;
 
 
 // Vertex Array Objects
-enum VAO_IDs { Triangles, Indicies, NumVAOs = 2 }; 
+enum VAO_IDs { Triangles, Indicies, NumVAOs = 4 }; 
 GLuint VAOs[NumVAOs]; 
 
 // Element Array Buffer
-enum Buffer_IDs { ArrayBuffer, NumBuffers = 4 }; 
+enum Buffer_IDs { ArrayBuffer, NumBuffers = 8 }; 
 GLuint Buffers[NumBuffers];
 
 // Shader program
 GLuint program;
+
+// Matrices
+mat4 view;
+mat4 projection;
+mat4 model;
+
+// Camera parameters
+vec3 cameraPosition = vec3(0.0f, 0.0f, 3.0f);
+vec3 cameraFront = vec3(0.0f, 0.0f, -1.0f);
+vec3 cameraUp = vec3(0.0f, 1.0f, 0.0f);
+
+
+// Player position
+vec3 playerPosition = vec3(0.0f, 0.0f, 0.0f);
+
+// Game timing
+float deltaTime = 0.0f;	
+float lastFrame = 0.0f;
+
+void createNewObject(float* objVertices, size_t vertexCount, unsigned int* objIndices, size_t indexCount, GLuint vaoIndex)
+{
+	// VAO generation and binding
+	glBindVertexArray(VAOs[vaoIndex]);
+
+	// Bind vertex positions
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[Triangles]);
+	glBufferData(GL_ARRAY_BUFFER, vertexCount, objVertices, GL_STATIC_DRAW);
+	// Bind indices
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Buffers[Indicies]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexCount, objIndices, GL_STATIC_DRAW);
+
+	// Vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//Unbinding
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
 
 int main()
 {
@@ -81,39 +121,43 @@ int main()
 	1, 2, 3 
 	};
 
+	float objectVertices[] = {     
+		0.3f, 0.3f, 0.0f,
+		0.3f, -0.3f, 0.0f,
+		-0.3f, -0.3f, 0.0f,
+		-0.3f, 0.3f, 0.0f
+	};
+
+	unsigned int objectIndices[] = {  
+		0, 1, 3, 
+		1, 2, 3 
+	};
+
 	// Set Colour
 	GLint colourLocation = glGetUniformLocation(program, "colourIn");
 	glUniform4f(colourLocation, 1.0f, 0.25f, 0.0f, 1.0f);
 
+
 	// VAO generation and binding
 	glGenVertexArrays(NumVAOs, VAOs);
-	glBindVertexArray(VAOs[0]);
 
 	// Buffer generation
 	glGenBuffers(NumBuffers, Buffers);
 
-	// Bind vertex positions
-	glBindBuffer(GL_ARRAY_BUFFER, Buffers[Triangles]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Create the rectangle object
+	createNewObject(vertices, sizeof(vertices), indices, sizeof(indices), 0);
+	// Create another object
+	///createNewObject(objectVertices, 12, objectIndices, 1);
 
-	// Bind indices
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Buffers[Indicies]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Vertex attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-
-
-	//Unbinding
-	glBindVertexArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Timing
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		// Input
 		processUserInput(window);
 
@@ -121,9 +165,26 @@ int main()
 		glClearColor(0.0f, 0.1f, 0.5f, 1.0f); 
 		glClear(GL_COLOR_BUFFER_BIT); 
 
+		// Model
+		model = mat4(1.0f);
+		model = translate(model, playerPosition); // Move to player position
+
+		// Camera
+		view = lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp);
+		projection = perspective(radians(45.0f), (float)windowWidth / (float)windowHeight, 0.1f, 100.0f);
+
+		// MVP (vertex shader uniform)
+		mat4 mvp = projection * view * model; 
+		GLint mvpLocation = glGetUniformLocation(program, "mvpIn");
+		glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, value_ptr(mvp));
+
 		//Drawing
 		glBindVertexArray(VAOs[0]);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // Draw the rectangle (Triangles, 6 indices, type, offset 0)
+
+		/*glBindVertexArray(VAOs[1]);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
+
 
 		// Swap buffers and poll events
 		glfwSwapBuffers(window); 
@@ -143,10 +204,28 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 // User inputs
 void processUserInput(GLFWwindow* WindowIn)
 {
+	// Movement speed
+	float movementSpeed = 1.0f * deltaTime; 
+
+	// Boundary
+
+
+
 	// Close window
 	if (glfwGetKey(WindowIn, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
 		glfwSetWindowShouldClose(WindowIn, true);
-	}
+
+	// W
+	if (glfwGetKey(WindowIn, GLFW_KEY_W) == GLFW_PRESS)
+		playerPosition += vec3(0.0f, movementSpeed, 0.0f);
+	// S
+	if (glfwGetKey(WindowIn, GLFW_KEY_S) == GLFW_PRESS)
+		playerPosition += vec3(0.0f, -movementSpeed, 0.0f);
+	// A
+	if (glfwGetKey(WindowIn, GLFW_KEY_A) == GLFW_PRESS)
+		playerPosition += vec3(-movementSpeed, 0.0f, 0.0f);
+	// D
+	if (glfwGetKey(WindowIn, GLFW_KEY_D) == GLFW_PRESS)
+		playerPosition += vec3(movementSpeed, 0.0f, 0.0f);
 }
 
--- Coursework2/coursework2/shaders/vertexShader.vert
+++ Coursework2/coursework2/shaders/vertexShader.vert
@@ -1,7 +1,8 @@
 #version 460
 layout (location = 0) in vec3 position;
+uniform mat4 mvpIn;
 
 void main() {
 
-    gl_Position = vec4(position.x, position.y, position.z, 1.0);
+    gl_Position = mvpIn * vec4(position.x, position.y, position.z, 1.0);
 }
